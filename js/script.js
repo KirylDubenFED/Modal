086e353b7631e2d944de5fbe34a45a818be3cbc4
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2020-11-10 01:10:29 +0300

    Add new persons and add additional columns

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+  
 import modalControl from './modal.js';
 
 const modal = modalControl();
@@ -9,7 +10,8 @@ let $ok = document.querySelector('.ok');
 const firstName = document.querySelector('#first_name');
 const lastName = document.querySelector('#last_name');
 const age = document.querySelector('#age');
-
+const gender = document.querySelector('#gender');
+const work = document.querySelector('#work');
 
 
 $ok.addEventListener('click', function () {
@@ -19,11 +21,13 @@ $ok.addEventListener('click', function () {
 			firstName: firstName.value,
 			lastName: lastName.value,
 			age: age.value,
+			gender: gender.value,
+			work: work.value,
 		}
 	)
 	let str = '';
 	for (let el of data) {
-		str += `<tr><td> ${el.firstName}</td><td>${el.lastName}</td><td>${el.age}</td></tr>`;
+		str += `<tr><td> ${el.firstName}</td><td>${el.lastName}</td><td>${el.age}</td><td>${el.gender}</td><td>${el.work}</td></tr>`;
 	}
 	$table.innerHTML = str;
 	modal.toggle('close');
@@ -47,29 +51,38 @@ let data = [
 		firstName: 'Ashton',
 		lastName: 'Kutcher',
 		age: 40,
-		gender: 'men',
-		hairСolor: 'black'
+		gender: 'Men',
+		work: 'Actor'
 	}, {
 		firstName: 'Bradley',
 		lastName: 'Pitt',
 		age: 54,
-		gender: 'men',
-		hairСolor: 'pink'
+		gender: 'Men',
+		work: 'Actor'
 	}, {
 		firstName: 'Hannah',
 		lastName: 'Dakota',
 		age: 24,
-		gender: 'women',
-		hairСolor: 'green'
+		gender: 'Women',
+		work: 'Single'
+	}, {
+		firstName: 'Roma',
+		lastName: 'Aheychik',
+		age: 13,
+		gender: 'Men',
+		work: 'Freelance'
+	}, {
+		firstName: 'Aleksey',
+		lastName: 'Bortnik',
+		age: 18,
+		gender: 'Men',
+		work: 'Student'
 	}
 ];
 
 let str = '';
 for (let el of data) {
-	str += `<tr><td> ${el.firstName}</td><td>${el.lastName}</td><td>${el.age}</td></tr>`;
+	str += `<tr><td> ${el.firstName}</td><td>${el.lastName}</td><td>${el.age}</td><td>${el.gender}</td><td>${el.work}</td></tr>`;
 	//  console.log (el)
 }
 $table.innerHTML = str;
-
-
-
